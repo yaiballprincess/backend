3d48095515710d1d2430568857189619a58e73c3
Able Arthy <ablearthy@gmail.com>
2023-06-18 23:53:04 +0300

    Clear imports

--- src/MyLib.hs
+++ src/MyLib.hs
@@ -1,27 +1,22 @@
 module MyLib (runApp) where
 
+import Network.Wai.Handler.Warp (run)
 import Servant
-import Servant.API
-import Servant.Server
 import Servant.Server.Generic
-import Servant.API.Generic
-import Network.Wai.Handler.Warp   (run)
 
 -- import qualified Data.Text as T
 import Data.Text.Encoding (encodeUtf8)
 
-import Control.Monad.Reader (ReaderT, runReaderT)
-import Control.Monad.IO.Class
+import Control.Monad.Reader (runReaderT)
 
 import Hasql.Connection qualified as Connection
 
 import Optics
 
+import Servant.Auth.Server (defaultCookieSettings, defaultJWTSettings)
 import YIBP.App
 import YIBP.Config (parseConfig)
 import YIBP.Server
-import Servant.Auth.Server (defaultJWTSettings, defaultCookieSettings)
-
 
 nt :: Env -> AppT Handler a -> Handler a
 nt e x = runReaderT x e
@@ -38,8 +33,9 @@ runApp = do
           (encodeUtf8 (dbConf ^. #password))
           (encodeUtf8 (dbConf ^. #db))
   Right conn <- Connection.acquire connSettings
-  let env = Env {dbConnection = conn, jwk = config ^. #jwk}
-  run 8080 $ genericServeTWithContext
-    (nt env)
-    theAPI
-    (defaultJWTSettings (config ^. #jwk) :. defaultCookieSettings :. EmptyContext)
+  let env = Env {dbConnection = conn, jwk = config ^. #jwk, appConfig = config}
+  run 8080 $
+    genericServeTWithContext
+      (nt env)
+      theAPI
+      (defaultJWTSettings (config ^. #jwk) :. defaultCookieSettings :. EmptyContext)
