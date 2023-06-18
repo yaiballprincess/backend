a8847483b5142453165ba8c23c95aecf30245490
Able Arthy <ablearthy@gmail.com>
2023-06-18 23:54:16 +0300

    Add some auth db functions

--- src/YIBP/Db/Auth.hs
+++ src/YIBP/Db/Auth.hs
@@ -1,10 +1,11 @@
-module YIBP.Db.Auth (insertRefreshToken) where
+module YIBP.Db.Auth (insertRefreshToken, getCreatedAtByRefreshToken, updateRefreshToken) where
 
 import Data.ByteString qualified as BS
 import Data.Int
 import Data.Text qualified as T
 import Data.Time qualified as Time
 import Data.UUID (UUID)
+import Data.Bifunctor
 
 import GHC.Stack (HasCallStack)
 
@@ -33,4 +34,45 @@ insertRefreshToken
   -> m (Maybe Time.UTCTime)
 insertRefreshToken uuid ownerId = do
   r <- withConn $ Session.run (insertRefreshTokenSession uuid ownerId)
+  liftError r
+
+getCreatedAtByRefreshTokenSession :: UUID -> Session.Session (Maybe Time.UTCTime)
+getCreatedAtByRefreshTokenSession uuid = Session.statement uuid stmt
+  where
+    stmt :: Statement UUID (Maybe Time.UTCTime)
+    stmt =
+      [TH.maybeStatement| 
+      select ("created_at" :: TIMESTAMPTZ) 
+      from "session" where "id" = ($1 :: uuid)
+    |]
+
+getCreatedAtByRefreshToken
+  :: (WithDb env m, HasCallStack)
+  => UUID
+  -> m (Maybe Time.UTCTime)
+getCreatedAtByRefreshToken uuid = do
+  r <- withConn $ Session.run (getCreatedAtByRefreshTokenSession uuid)
+  liftError r
+
+updateRefreshTokenSession :: UUID -> UUID -> Session.Session (Maybe (Time.UTCTime, Int))
+updateRefreshTokenSession old new = fmap (second fromIntegral) <$> Session.statement (old, new) stmt
+  where
+    stmt :: Statement (UUID, UUID) (Maybe (Time.UTCTime, Int32))
+    stmt =
+      [TH.maybeStatement|
+      update "session"
+      set 
+        "id" = ($2 :: uuid),
+        "created_at" = now()
+      where "id" = ($1 :: uuid)
+      returning ("created_at" :: TIMESTAMPTZ), ("owner_id" :: int4)
+    |]
+
+updateRefreshToken
+  :: (WithDb env m, HasCallStack)
+  => UUID
+  -> UUID
+  -> m (Maybe (Time.UTCTime, Int))
+updateRefreshToken old new = do
+  r <- withConn $ Session.run (updateRefreshTokenSession old new)
   liftError r
