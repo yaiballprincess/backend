5c4f5a9d17e7e4e14951b8b448d8ccb0e809df03
Able Arthy <ablearthy@gmail.com>
2023-07-02 02:27:13 +0300

    Add IdObject

--- src/YIBP/Core/Id.hs
+++ src/YIBP/Core/Id.hs
@@ -1,8 +1,19 @@
+{-# LANGUAGE OverloadedStrings #-}
 module YIBP.Core.Id where
 
 import GHC.Generics
 import Data.Aeson
+import Data.Aeson.KeyMap qualified as J
+import Servant
 
 newtype Id a = Id Int
   deriving (Show, Eq, Generic)
-  deriving newtype (FromJSON, ToJSON)
+  deriving newtype (FromJSON, ToJSON, FromHttpApiData)
+
+data IdObject a = IdObject Int a
+  deriving (Show, Eq)
+
+instance ToJSON a => ToJSON (IdObject a) where
+  toJSON (IdObject oId obj) = case toJSON obj of
+    Object o -> toJSON $ J.insert "id" (toJSON oId) o
+    other -> object [ "id" .= oId, "value" .= other ]
