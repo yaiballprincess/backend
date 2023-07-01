4313d7b73cf49ecbb1af0218844f0d3cc7fb2413
Able Arthy <ablearthy@gmail.com>
2023-07-02 02:27:40 +0300

    Add Core.PollTemplate

---
+++ src/YIBP/Core/PollTemplate.hs
@@ -0,0 +1,50 @@
+module YIBP.Core.PollTemplate
+  ( PollTemplateOption (..)
+  , getPollTemplateOption
+  , PollTemplate (..)
+  , CreatePollTemplate (..)
+  , UpdatePollTemplate
+  , PollTemplateFull (..)
+  ) where
+
+import Data.Aeson
+import Data.Text qualified as T
+import Data.Time
+import Data.Vector qualified as V
+import GHC.Generics
+import YIBP.Core.Id
+
+newtype PollTemplateOption = PollTemplateOption T.Text
+  deriving newtype (FromJSON, ToJSON)
+
+getPollTemplateOption :: PollTemplateOption -> T.Text
+getPollTemplateOption (PollTemplateOption t) = t
+
+data PollTemplate = PollTemplate
+  { isMultiple :: !Bool
+  , isAnonymous :: !Bool
+  , endsAt :: !(Maybe UTCTime)
+  , question :: !T.Text
+  , options :: !(V.Vector PollTemplateOption)
+  }
+  deriving (Generic, FromJSON, ToJSON)
+
+data CreatePollTemplate = CreatePollTemplate
+  { isMultiple :: !Bool
+  , isAnonymous :: !Bool
+  , endsAt :: !(Maybe UTCTime)
+  , question :: !T.Text
+  }
+  deriving (Generic, FromJSON, ToJSON)
+
+data PollTemplateFull = PollTemplateFull
+  { id :: !(Id PollTemplate)
+  , isMultiple :: !Bool
+  , isAnonymous :: !Bool
+  , endsAt :: !(Maybe UTCTime)
+  , question :: !T.Text
+  , options :: !(V.Vector (IdObject PollTemplateOption))
+  }
+  deriving (Generic, ToJSON)
+
+type UpdatePollTemplate = CreatePollTemplate
