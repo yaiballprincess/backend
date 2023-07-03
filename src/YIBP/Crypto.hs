68d2a04b5d26a7520dd9e6b55a7558206486e92a
Able Arthy <ablearthy@gmail.com>
2023-07-03 20:29:04 +0300

    Add Encrypted

--- src/YIBP/Crypto.hs
+++ src/YIBP/Crypto.hs
@@ -1,7 +1,15 @@
-module YIBP.Crypto (encrypt, decrypt, encryptRandom) where
+module YIBP.Crypto
+  ( encrypt
+  , decrypt
+  , encryptRandom
+  , Encrypted
+  , encryptedToByteString
+  , byteStringToEncrypted
+  ) where
 
 import YIBP.Crypto.TH
 
+import Data.ByteArray qualified as BA
 import Data.ByteString qualified as BS
 
 import Crypto.Cipher.AES (AES256)
@@ -17,18 +25,32 @@ cryptoCipher = case cipherInit cryptoKey of
   CryptoFailed err -> error $ "unexpected error: Unable to initialize cipher: " <> show err
   CryptoPassed x -> x
 
+unsafeMakeIV :: BS.ByteString -> IV AES256
+unsafeMakeIV =
+  makeIV >>= \case
+    Just iv -> pure iv
+    Nothing -> error "unexpected error: unable to makeIV"
+
 genRandomIV :: (CRT.MonadRandom m) => m (IV AES256)
 genRandomIV = do
   bytes :: BS.ByteString <- CRT.getRandomBytes $ blockSize (undefined :: AES256)
-  case makeIV bytes of
-    Just iv -> pure iv
-    Nothing -> error "unexpected error: unable to generate random IV"
+  pure $ unsafeMakeIV bytes
 
 encrypt, decrypt :: IV AES256 -> BS.ByteString -> BS.ByteString
 encrypt = ctrCombine cryptoCipher
 decrypt = encrypt
 
-encryptRandom :: (CRT.MonadRandom m) => BS.ByteString -> m (IV AES256, BS.ByteString)
+encryptRandom :: (CRT.MonadRandom m) => BS.ByteString -> m Encrypted
 encryptRandom msg = do
   iv <- genRandomIV
-  pure (iv, encrypt iv msg)
+  pure $ Encrypted iv (encrypt iv msg)
+
+data Encrypted = Encrypted !(IV AES256) !BS.ByteString
+
+encryptedToByteString :: Encrypted -> BS.ByteString
+encryptedToByteString (Encrypted iv bs) = BS.append (BA.convert iv) bs
+
+byteStringToEncrypted :: BS.ByteString -> Encrypted
+byteStringToEncrypted bs = Encrypted (unsafeMakeIV iv) payload
+  where
+    (iv, payload) = BS.splitAt (blockSize (undefined :: AES256)) bs
