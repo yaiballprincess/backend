b281562ade965d7de0bf34241cba9b590402396d
Able Arthy <ablearthy@gmail.com>
2023-07-03 17:35:15 +0300

    Remove unnecessary imports

--- src/MyLib.hs
+++ src/MyLib.hs
@@ -7,21 +7,14 @@ import Servant.Server.Generic
 -- import qualified Data.Text as T
 import Data.Text.Encoding (encodeUtf8)
 
-import Control.Monad.Reader (runReaderT)
-
 import Optics
 
 import Control.Concurrent
-import Data.Vector qualified as V
-import GHC.Generics
 import Hasql.Connection
 import Hasql.Connection qualified as Connection
 import Servant.Auth.Server (defaultCookieSettings, defaultJWTSettings)
-import Servant.Auth.Server.Internal.AddSetCookie
-import YIBP.App
 import YIBP.Config
 import YIBP.Db
-import YIBP.Db.Receiver
 import YIBP.Scheduler.Scheduler
 import YIBP.Server
 
