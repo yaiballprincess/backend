1e7ea8a968949df97a755d7b7e22c3f699bf8794
Able Arthy <ablearthy@gmail.com>
2023-08-04 21:07:43 +0300

    Add help

--- app/Main.hs
+++ app/Main.hs
@@ -8,9 +8,18 @@ import MyLib qualified (runApp)
 import YIBP.CLI.CreateAdmin (createAdmin)
 import YIBP.CLI.Migrate (doMigrations)
 
+helpMessage :: String
+helpMessage = "Usage: ./program-name [ACTION]\n\
+              \  [ACTION] \n\
+              \    help - show help \n\
+              \    migrate - run migrations \n\
+              \    create-admin - create user 'admin' \n\
+              \    <otherwise> - run application"
+
 main :: IO ()
 main = do
   getArgs >>= \case
+    ("help" : _) -> putStrLn helpMessage
     ("migrate" : _) -> doMigrations
     ("create-admin" : _) -> createAdmin
     _ -> MyLib.runApp
